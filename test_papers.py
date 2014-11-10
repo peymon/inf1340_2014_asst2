1d176ed23317f7406966eca09f186f89689d2947
Peymon <peymon@gmail.com>
2014-11-09 19:58:06 -0500

    Need more test cases

--- test_papers.py
+++ test_papers.py
@@ -2,10 +2,10 @@
 
 """ Module to test papers.py  """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Peymon'
+__email__ = "peymon@peymon.com"
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 Peymon"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
@@ -28,6 +28,7 @@ def test_files():
     with pytest.raises(FileNotFoundError):
         decide("test_returning_citizen.json", "", "countries.json")
 
-# add functions for other tests
+def test_files():
+    with pytest.raises(FileNotFoundError):
+        decide("test_returning_citizen.json", "watchlist.json", "")
 
-print (test_basic())
